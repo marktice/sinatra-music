1792fae58c2b4cbca5342c85f9ca58be94763530
Mark Tice <marktice@protonmail.com>
2018-03-27 14:37:46 +1100

    Initial commit

---
+++ myapp.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
+get '/' do
+  @title = 'Welcome to Mark\'s App!'
+  erb :index
+end
